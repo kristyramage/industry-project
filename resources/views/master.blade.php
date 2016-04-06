8071ed1e127d23929cd5bcb8489875983f9ccefc
Kristy Ramage <kristy.ramage@gmail.com>
2016-04-07 11:28:40 +1200

    Implemented the hamburger menu, fixed container, added border top to footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,7 +13,11 @@
 		<div class="col-sm-12 text-center">
 			<img src="#" alt="Captured write logo">
 		</div>
-		<nav class="navbar">
+		
+
+
+
+<!-- 		<nav class="navbar">
 			<ul>
 				<li><a href="/">home</a></li>
 				<li><a href="about">about</a></li>
@@ -21,7 +25,35 @@
 				<li><a href="gifts">gifts</a></li>
 				<li><a href="contact">contact</a></li>
 			</ul>
+		</nav> -->
+
+
+
+		<nav class="navbar navbar-static-top" role="navigation">
+		    <div class="container">
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		        </div>
+
+		        <!-- Collect the nav links, forms, and other content for toggling -->
+		        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		            <ul class="nav">
+		                <li><a href="/">home</a></li>
+						<li><a href="about">about</a></li>
+						<li><a href="shop">shop</a></li>
+						<li><a href="gifts">gifts</a></li>
+						<li><a href="contact">contact</a></li>
+		            </ul>
+		        </div>
+		    </div>
 		</nav>
+
+
 		
 		<div>
 			@yield('content')
